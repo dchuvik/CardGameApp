9ed156d7ee4f05b3004e0dbe92d48ff966e69c1b
David Chuvik <dchuvik@gmail.com>
2023-02-27 09:15:24 -0500

    card game practicing access modifiers, abstract classes, and method overridng

---
+++ CardGame/Models/CardValue.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+
+
+
+public enum CardValue
+{
+    Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+}
---
+++ CardGame/Models/PlayingCardModel.cs
@@ -0,0 +1,9 @@
+﻿
+
+
+
+public class PlayingCardModel
+{
+    public CardSuit Suit { get; set; }
+    public CardValue Value { get; set; }
+}
